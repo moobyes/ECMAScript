57d7145c640706274269317f7504afe05134160d
Moobye <moobyes@gmail.com>
2021-08-13 07:52:35 +0000

    增加contantrol

---
+++ Algorithm/flatObject.js
@@ -0,0 +1,85 @@
+/*
+ * @Description: 扁平
+ * @Author: Moobye
+ * @Date: 2021-08-13 11:58:38
+ * @LastEditTime: 2021-08-13 14:19:22
+ * @LastEditors: Moobye
+ */
+let arr = [
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+]
+
+// change to like this:
+let obj = [
+  {
+      "id": 1,
+      "name": "部门1",
+      "pid": 0,
+      "children": [
+          {
+              "id": 2,
+              "name": "部门2",
+              "pid": 1,
+              "children": []
+          },
+          {
+              "id": 3,
+              "name": "部门3",
+              "pid": 1,
+              "children": [
+                  // 结果 ,,,
+              ]
+          }
+      ]
+  }
+]
+
+
+let arr1 = [
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+]
+
+function arrayToTree(items) {
+  const result = [];   // 存放结果集
+  const itemMap = {};  // 
+  for (const item of items) {
+    const id = item.id;
+    const pid = item.pid;
+
+    if (!itemMap[id]) {
+      itemMap[id] = {
+        children: [],
+      }
+    }
+
+    itemMap[id] = {
+      ...item,
+      children: itemMap[id]['children']
+    }
+
+    const treeItem =  itemMap[id];
+
+    if (pid === 0) {
+      result.push(treeItem);
+    } else {
+      if (!itemMap[pid]) {
+        itemMap[pid] = {
+          children: [],
+        }
+      }
+      itemMap[pid].children.push(treeItem)
+    }
+
+  }
+  return result;
+}
+
+console.log(arrayToTree(arr1))
